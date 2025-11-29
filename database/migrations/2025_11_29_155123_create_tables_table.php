49c9f6bfad9108ecf238023b84d3a3188e7449b9
kang-woo <mhmd.aryf.edu@gmail.com>
2025-11-29 23:00:36 +0700

    migration

---
+++ database/migrations/2025_11_29_155123_create_tables_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // misal "Meja 1"
+            $table->integer('capacity')->default(4); // optional
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
---
+++ database/migrations/2025_11_29_155252_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('table_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // waiter
+            $table->string('customer_name'); // nama tamu / pemesan
+            $table->string('customer_phone')->nullable(); // opsional
+            $table->string('status')->default('pending'); // pending, proses, selesai
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
